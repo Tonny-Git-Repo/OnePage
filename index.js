9a3c83a450b237ade4266507c98cb9b2eb27b5b6
Tonny Piper <pipertonny@gmail.com>
2021-11-27 21:06:56 +0100

    starting to make the One Pager responsive

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ document.querySelectorAll('.story-btn').forEach( btn=>{
         btn.nextElementSibling.classList.toggle('change')
     })
 })
+
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
